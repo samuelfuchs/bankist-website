4e6c65b33effb56a63762a112fc1046089243aa4
Samuel Fuchs <fuchsgerson@gmail.com>
2021-06-19 14:32:58 -0300

    Selecting, Creating, and Deleting Elements

--- script.js
+++ script.js
@@ -29,3 +29,17 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
