54e52be22c5b4ae5663e2a80f35fda002bf72603
Samuel Fuchs <fuchsgerson@gmail.com>
2021-06-20 19:48:57 -0300

    Styles, Attributes and Classes

--- script.js
+++ script.js
@@ -64,3 +64,46 @@ document
   .addEventListener('click', function () {
     message.remove();
   });
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+console.log(getComputedStyle(message).height);
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+logo.alt = 'Beautiful minimalist logo';
+logo.setAttribute('company', 'Bankist');
+console.log(logo.getAttribute('company'));
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.twitter-link');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not called includes (like in js)
+
+// Dont use - this overrides all classes
+logo.className = 'jonas';
