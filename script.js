cdbafeea7f38208e606c17571794c5ec53c9a9af
Samuel Fuchs <fuchsgerson@gmail.com>
2021-06-21 00:49:46 -0300

    Types of Events and Event Handlers

--- script.js
+++ script.js
@@ -138,3 +138,17 @@ btnScrollTo.addEventListener('click', function (e) {
 
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// };
